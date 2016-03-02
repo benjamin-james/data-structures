0a809eef91c7ae184e8e160c1fa45477ab555eda
Benjamin James <bjames@openmailbox.org>
2016-03-01 21:05:51 -0600

    updated README, added filenames to main
    Echelon Treasury Authorities Juarez M.P.R.I. Tijuana AMTRAK Power lines
    MP5K-SD MARTA ISACA Looting interception CONUS Operation Iraqi Freedom

--- hw2.go
+++ hw2.go
@@ -8,10 +8,14 @@ import (
 )
 
 func main() {
-
 	filename := "/dev/stdin"
+	uni_file, bi_file, cp_file, time_file := "uni", "bi", "cp", "time"
 	if len(os.Args) > 1 {
 		filename = os.Args[1]
+		uni_file = fmt.Sprintf("%s.%s", filename, uni_file)
+		bi_file = fmt.Sprintf("%s.%s", filename, bi_file)
+		cp_file = fmt.Sprintf("%s.%s", filename, cp_file)
+		time_file = fmt.Sprintf("%s.%s", filename, time_file)
 	}
 	uni, bi, result := tree.NewAVL(), tree.NewAVL(), tree.NewAVL()
 	err := text.ReadFile(filename, uni, bi)
@@ -20,5 +24,4 @@ func main() {
 		return
 	}
 	text.ComputeProbabilities(uni, bi, result)
-	result.Display()
 }

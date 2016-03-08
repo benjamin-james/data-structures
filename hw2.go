f207c4bd32cf4ed0ee67b5f33d042c083331be12
Benjamin James <bjames@openmailbox.org>
2016-03-07 19:28:33 -0600

    does timing and also does both BST and AVL
    Dolch FXR Michoacna Epidemic DCJFTF Harvard Kh-11 BATF Norvo Virus Black
    out satellite imagery Torreon H&K Medco Wackendude

--- hw2.go
+++ hw2.go
@@ -3,9 +3,19 @@ package main
 import (
 	text "benJames/text"
 	tree "benJames/tree"
+	util "benJames/util"
 	"fmt"
 	"os"
+	"time"
 )
+
+func compute(filename string, uni, bi, result util.DataStructure) (time.Duration, int) {
+	now := time.Now()
+	wc, err := text.ReadFile(filename, uni, bi)
+	assert(err)
+	text.ComputeProbabilities(uni, bi, result)
+	return time.Since(now), wc
+}
 
 func main() {
 	filename := "/dev/stdin"
@@ -17,12 +27,15 @@ func main() {
 		cp_file = fmt.Sprintf("%s.%s", filename, cp_file)
 		time_file = fmt.Sprintf("%s.%s", filename, time_file)
 	}
-	uni, bi, result := tree.NewAVL(), tree.NewAVL(), tree.NewAVL()
-	assert(text.ReadFile(filename, uni, bi))
-	assert(text.DumpToFile(uni_file, uni))
-	assert(text.DumpToFile(bi_file, bi))
-	text.ComputeProbabilities(uni, bi, result)
-	assert(text.DumpToFile(cp_file, result))
+	uni_avl, bi_avl, result_avl := tree.NewAVL(), tree.NewAVL(), tree.NewAVL()
+	uni_bst, bi_bst, result_bst := tree.NewBST(), tree.NewBST(), tree.NewBST()
+	dur_avl, wc := compute(filename, uni_avl, bi_avl, result_avl)
+	dur_bst, _ := compute(filename, uni_bst, bi_bst, result_bst)
+	assert(text.DumpToFile(uni_file, uni_avl))
+	assert(text.DumpToFile(bi_file, bi_avl))
+	assert(text.DumpToFile(cp_file, result_avl))
+	s := fmt.Sprintf("filename:\t%s\nword count:\t%d\nAVL time:\t%s\nBST time:\t%s", filename, wc, dur_avl.String(), dur_bst.String())
+	text.WriteString(time_file, s)
 }
 
 // similar to assert(3)
--- text/io.go
+++ text/io.go
@@ -3,6 +3,7 @@ package text
 import (
 	u "benJames/util"
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -13,15 +14,25 @@ func Clean(str string) string {
 	return str
 }
 
-func ReadFile(filename string, unigrams, bigrams u.DataStructure) error {
-	file, err := os.Open(filename)
+func WriteString(filename string, s string) error {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return err
+	}
+	defer file.Close()
+	_, err = fmt.Fprintln(file, s)
+	return err
+}
+func ReadFile(filename string, unigrams, bigrams u.DataStructure) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
 	prev := ""
+	count := 0
 	for scanner.Scan() {
 		str := scanner.Text()
 		str = Clean(str)
@@ -30,8 +41,9 @@ func ReadFile(filename string, unigrams, bigrams u.DataStructure) error {
 			bigrams.Insert(NewBi(prev, str, 1))
 		}
 		prev = str
+		count++
 	}
-	return scanner.Err()
+	return count, scanner.Err()
 }
 
 func DumpToFile(filename string, ds u.DataStructure) error {

8c756beca9e29bca5e045942ead7b78eacce80b3
Benjamin James <bjames@openmailbox.org>
2016-03-07 21:29:25 -0600

    added cli parsing, added optional avl/bst
    lynch Afghanistan Osama Suspicious device UNCPCJ crypto anarchy Critical
    Infrastructure & Key Resources Smuggling 64 Vauxhall Cross TSCI beanpole
    password Failure CNCIS COCOT

--- hw2.go
+++ hw2.go
@@ -6,8 +6,66 @@ import (
 	util "benJames/util"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
+
+func main() {
+	filename := "/dev/stdin"
+	uni_file, bi_file, cp_file, time_file := "uni", "bi", "cp", "time"
+	str := ""
+	use_bst := true
+	use_avl := true
+	has_input := false
+	for argc, argv := range os.Args {
+		if argc == 0 {
+			continue
+		} else if strings.Compare("-h", argv) == 0 || strings.Compare("--help", argv) == 0 {
+			fmt.Printf("Usage: %s [--no-bst|--no-avl] filename\n", os.Args[0])
+			os.Exit(0)
+		} else if strings.Compare("--no-bst", argv) == 0 {
+			use_bst = false
+		} else if strings.Compare("--no-avl", argv) == 0 {
+			use_avl = false
+		} else if _, err := os.Stat(argv); err == nil && !has_input {
+			filename = argv
+			has_input = true
+			uni_file = fmt.Sprintf("%s.%s", filename, uni_file)
+			bi_file = fmt.Sprintf("%s.%s", filename, bi_file)
+			cp_file = fmt.Sprintf("%s.%s", filename, cp_file)
+			time_file = fmt.Sprintf("%s.%s", filename, time_file)
+		} else if !has_input {
+			fmt.Fprintf(os.Stderr, "The file \"%s\" does not exist.\n", argv)
+			os.Exit(1)
+		} else {
+			fmt.Fprintf(os.Stderr, "Bad argument \"%s\"\n", argv)
+			os.Exit(1)
+		}
+	}
+	if use_avl {
+		uni, bi, result := tree.NewAVL(), tree.NewAVL(), tree.NewAVL()
+		dur, wc := Compute(filename, uni, bi, result)
+		if use_bst {
+			uni_bst, bi_bst, result_bst := tree.NewBST(), tree.NewBST(), tree.NewBST()
+			dur_bst, _ := Compute(filename, uni_bst, bi_bst, result_bst)
+			str = fmt.Sprintf("filename:\t%s\nword count:\t%d\nAVL time:\t%s\nBST time:\t%s\n", filename, wc, dur.String(), dur_bst.String())
+		} else {
+			str = fmt.Sprintf("filename:\t%s\nword count:\t%d\nAVL time:\t%s\n", filename, wc, dur.String())
+		}
+		text.DumpToFile(uni_file, uni)
+		text.DumpToFile(bi_file, bi)
+		text.DumpToFile(cp_file, result)
+		text.WriteString(time_file, str)
+	} else if use_bst {
+		uni, bi, result := tree.NewBST(), tree.NewBST(), tree.NewBST()
+		dur, wc := Compute(filename, uni, bi, result)
+		str = fmt.Sprintf("filename:\t%s\nword count:\t%d\nBST time:\t%s\n", filename, wc, dur.String())
+		text.DumpToFile(uni_file, uni)
+		text.DumpToFile(bi_file, bi)
+		text.DumpToFile(cp_file, result)
+		text.WriteString(time_file, str)
+	}
+}
 
 func Compute(filename string, uni, bi, result util.DataStructure) (time.Duration, int) {
 	now := time.Now()
@@ -15,27 +73,6 @@ func Compute(filename string, uni, bi, result util.DataStructure) (time.Duration
 	assert(err)
 	text.ComputeProbabilities(uni, bi, result)
 	return time.Since(now), wc
-}
-
-func main() {
-	filename := "/dev/stdin"
-	uni_file, bi_file, cp_file, time_file := "uni", "bi", "cp", "time"
-	if len(os.Args) > 1 {
-		filename = os.Args[1]
-		uni_file = fmt.Sprintf("%s.%s", filename, uni_file)
-		bi_file = fmt.Sprintf("%s.%s", filename, bi_file)
-		cp_file = fmt.Sprintf("%s.%s", filename, cp_file)
-		time_file = fmt.Sprintf("%s.%s", filename, time_file)
-	}
-	uni_avl, bi_avl, result_avl := tree.NewAVL(), tree.NewAVL(), tree.NewAVL()
-	uni_bst, bi_bst, result_bst := tree.NewBST(), tree.NewBST(), tree.NewBST()
-	dur_avl, wc := Compute(filename, uni_avl, bi_avl, result_avl)
-	dur_bst, _ := Compute(filename, uni_bst, bi_bst, result_bst)
-	assert(text.DumpToFile(uni_file, uni_avl))
-	assert(text.DumpToFile(bi_file, bi_avl))
-	assert(text.DumpToFile(cp_file, result_avl))
-	s := fmt.Sprintf("filename:\t%s\nword count:\t%d\nAVL time:\t%s\nBST time:\t%s", filename, wc, dur_avl.String(), dur_bst.String())
-	text.WriteString(time_file, s)
 }
 
 // similar to assert(3)
